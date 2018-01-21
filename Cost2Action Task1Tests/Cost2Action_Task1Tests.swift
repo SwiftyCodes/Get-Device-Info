14ccda81167e1c002d68c1ce9fd70a68d939cf2d
Chaudhary Himanshu Raj <chaudharyhimanshuraj@chaudharys-mac.local>
2018-01-21 17:45:23 +0530

    Initial Commit

---
+++ Cost2Action Task1Tests/Cost2Action_Task1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Cost2Action_Task1Tests.swift
+//  Cost2Action Task1Tests
+//
+//  Created by Chaudhary Himanshu Raj on 21/01/18.
+//  Copyright © 2018 Chaudhary Himanshu Raj. All rights reserved.
+//
+
+import XCTest
+@testable import Cost2Action_Task1
+
+class Cost2Action_Task1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
